036b24cb47afb44e6228dec89e37c610df8d3b0e
Dinko Ivanov <dinko.ivanov@gmail.com>
2019-05-16 15:55:02 +0300

    template-forms

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -15,9 +15,22 @@ export class WidgetsComponent implements OnInit {
 
   ngOnInit() {
     this.widgets = this.widgetsService.widgets;
+    this.reset();
   }
 
   selected(widget: Widget) {
     this.selectedWidget = widget;
   }
+
+  reset() {
+    this.selectedWidget = { id: null, name: '', description: ''};
+  }
+
+  saved(widget: Widget) {
+    console.log('SAVING', widget);
+  }
+
+  cancelled(widget: Widget) {
+    this.reset();
+}
 }
